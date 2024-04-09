c46c1d13c26311a45e1ddb8a18c364d32263ddd7
sangjun1126 <jeongsangjun1234@gmail.com>
2024-04-09 16:44:40 +0900

    초기 홈페이지 1차 작업중

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>마이큐 당구장</h1>
       </header>
+
+      <main>
+        <p>당구장 홈페이지를 만들어보세요!</p>
+      </main>
+
+      <footer>
+        <p>&copy : 2024 마이큐 홈페이지</p>
+      </footer>
     </div>
-  );
+  )
 }
 
 export default App;
